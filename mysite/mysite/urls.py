493c808215147382d8cd3b03daaee6363901c78a
Alexander Shelton <alexander.nathan.shelton@outlook.com>
2024-01-23 02:04:53 -0500

    DJ4E Updates

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,24 +1,44 @@
 import os
 from django.contrib import admin
-from django.urls import include, path, re_path
+from django.urls import path, include, re_path
+from django.conf import settings
+from django.contrib.auth import views as auth_views
 from django.views.static import serve
 from django.views.generic import TemplateView
 
-
-# Up two folders to serve "site" content
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-SITE_ROOT = os.path.join(BASE_DIR, 'site')
-
 urlpatterns = [
-    path('hello', include('hello.urls')),
+    path('', include('home.urls')),  # Change to ads.urls
     path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls')),
-    path('autos/', include('autos.urls')),
-    path('cats/', include('cats.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('', TemplateView.as_view(template_name='home/main.html')),                                                                                           
+    path('ads/', include('ads.urls')),
+    re_path(r'^oauth/', include('social_django.urls', namespace='social')),
+]
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+urlpatterns += [
     re_path(r'^site/(?P<path>.*)$', serve,
-        {'document_root': SITE_ROOT, 'show_indexes': True},
+        {'document_root': os.path.join(BASE_DIR, 'site'),
+         'show_indexes': True},
         name='site_path'
+        ),
+]
+
+urlpatterns += [
+    path('favicon.ico', serve, {
+            'path': 'favicon.ico',
+            'document_root': os.path.join(BASE_DIR, 'home/static'),
+        }
     ),
 ]
+
+try:
+    from . import github_settings
+    social_login = 'registration/login_social.html'
+    urlpatterns.insert(0,
+                       path('accounts/login/', auth_views.LoginView.as_view(template_name=social_login))
+                       )
+    print('Using', social_login, 'as the login template')
+except:
+    print('Using registration/login.html as the login template')
+
+
