7b5051fbf8c5b1751ae18df56b30ea2275013b66
Alexander Shelton <alexander.nathan.shelton@outlook.com>
2024-01-26 20:31:10 -0500

    Week 2 Dj4E

--- django_projects/mysite/ads/views.py
+++ django_projects/mysite/ads/views.py
@@ -1,6 +1,12 @@
-from ads.models import Ad
+from django.views import View
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy, reverse
+from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from ads.owner import OwnerListView, OwnerDetailView, OwnerCreateView, OwnerUpdateView, OwnerDeleteView
 
+from ads.models import Ad
 
 class AdListView(OwnerListView):
     model = Ad
@@ -8,11 +14,30 @@ class AdListView(OwnerListView):
 
 class AdDetailView(OwnerDetailView):
     model = Ad
+    template_name = 'ads/ad_detail.html'
 
 
 class AdCreateView(OwnerCreateView):
-    model = Ad
-    fields = ['title','price', 'text']
+    template_name = 'ads/ad_form.html'
+    success_url = reverse_lazy('ads:all')
+
+    def get(self, request, pk=None):
+        form = CreateForm()
+        ctx = {'form': form}
+        return render(request, self.template_name, ctx)
+    
+    def post(self, request, pk=None):
+        form = CreateForm(request.POST, request.FILES or None)
+
+        if not form.is_valid():
+            ctx = {'form': form}
+            return render(request, self.template_name.ctx)
+        
+        pic = form.save(commit=False)
+        pic.owner = self.request_name, ctx
+        pic.save()
+        return redirect(self.sucess_url)
+
 
 
 class AdUpdateView(OwnerUpdateView):
